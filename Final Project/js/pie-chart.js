46fb87943f49551288ccbbcb836359a574060686
Urwa Fatima <urwa.fatima.raza983@gmail.com>
2023-01-13 13:12:03 +0100

    fixed the responsive issue for pie chart

--- Final Project/js/pie-chart.js
+++ Final Project/js/pie-chart.js
@@ -2,43 +2,32 @@ render();
 window.addEventListener('resize', render);
 
 function render() {
-    var $container = $('#world-asia-pie-chart'),
-        τ = 2 * Math.PI,
-        width = $container.width(),
-        height = $container.height();
     const divID = d3.select('#world-asia-pie-chart');
-    myResponsiveComponent(divID, Object.assign({}, {
-        // width: +divID.node().getBoundingClientRect().width,
-        // height: +divID.node().getBoundingClientRect().height,
-        width: width,
-        height: height,
+    pie_chart(divID, Object.assign({}, {
+        width: divID.node().getBoundingClientRect().width,
+        height: divID.node().getBoundingClientRect().height,
         margin: { top: 0, bottom: 0, left: 0, right: 0 }
     }));
 
 }
-function myResponsiveComponent(selection, props) {
+function pie_chart(selection, props) {
 
     const { width,
         height,
         margin
     } = props;
 
-    var radius = Math.min(width, height) / 2 - margin;
-
-    console.log(width, height)
+    var radius = Math.min(width, height) / 2 - margin.left - margin.right;
 
     let svg = selection.selectAll('svg').data([null]);
 
     svg = svg.enter().append('svg')
         .merge(svg)
-        .attr("width", '100%')
-        .attr("height", '100%')
+        .attr("class", "svg-content")
         .attr('viewBox', '0 0 ' + Math.min(width, height) + ' ' + Math.min(width, height));
-    //     .append("g")
-    //     .attr("transform", "translate(" + Math.min(width, height) / 2 + "," + Math.min(width, height) / 2 + ")");
-    // ;
 
-    // const { g, innerwidth, innerheight } = marginConvention(svg, { width, height, margin });
+    // Remove previous group element
+    svg.selectAll("g").remove();
 
     let g = svg.selectAll('g').data([null]);
     g = g
@@ -49,14 +38,6 @@ function myResponsiveComponent(selection, props) {
         .attr('height', height)
         .attr("transform", "translate(" + Math.min(width, height) / 2 + "," + Math.min(width, height) / 2 + ")");
 
-    // let rect = g.selectAll('rect').data([null]);
-    // rect = rect
-    //     .enter()
-    //     .append('rect')
-    //     .attr('rx', 50)
-    //     .merge(rect)
-    //     .attr('width', innerwidth)
-    //     .attr('height', innerheight);
 
     //CSV Data
     // d3.csv("../../data_clean/monthly_data.csv").then(function (data) {
@@ -70,8 +51,8 @@ function myResponsiveComponent(selection, props) {
     const data = { "a": 9, "b": 20, "c": 30, "d": 8, "e": 12, "f": 4, h: 10 }
 
     //color 
-    var colorList = ["#ff595e", "#ffca3a", "#dd6e42", "#6a4c93",
-        "#8ac926", "#1982c4", "#8f2d56"];
+    var colorList = ["#FF1D33", "#74234D", "#dd6e42", "#6a4c93",
+        "#8ac926", "#26265C", "#8f2d56"];
 
     const color = d3.scaleOrdinal()
         .range(colorList);
@@ -79,6 +60,7 @@ function myResponsiveComponent(selection, props) {
     // Compute the position of each group on the pie:
     const pie = d3.pie()
         .value(function (d) { return d[1] })
+        .sortValues((a, b) => a < b ? 1 : -1);
 
     const data_ready = pie(Object.entries(data))
     // Now I know that group A goes from 0 degrees to x degrees and so on.
@@ -89,48 +71,37 @@ function myResponsiveComponent(selection, props) {
         .outerRadius(radius)
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
-        .selectAll('mySlices')
+
+    // var paths = svg.selectAll('mySlices').data([null]);
+    // paths = paths
+    //     .enter()
+
+    // g.selectAll("path").remove();
+    // g.selectAll("text").remove();
+
+    // let paths = g.selectAll('mySlices').data([null]);
+
+    // paths = paths
+    g.selectAll('mySlices')
         .data(data_ready)
         .join('path')
         .attr('d', arcGenerator)
         .attr('fill', function (d) { return (color(d.data[0])) })
         .attr("stroke", "white")
         .style("stroke-width", "1px")
-        .style("opacity", 0.9)
+        .style("opacity", 0.9);
 
     // Now add the annotation. Use the centroid method to get the best coordinates
-    svg
-        .selectAll('mySlices')
+    g.selectAll('mySlices')
         .data(data_ready)
         .join('text')
         .text(function (d) { return "grp " + d.data[0] })
         .attr("transform", function (d) { return `translate(${arcGenerator.centroid(d)})` })
         .style("text-anchor", "middle")
-        .style("font-size", 17)
+        .style("font-size", "1.25em")
 
-    //close of Data function from csv
+    // close of Data function from csv
     // });
-
-
-}
-function marginConvention(selection, props) {
-    const { width, height, margin, className = "margin-group" } = props;
-    let g = selection
-        .selectAll('.' + className).data([null]);
-    g = g.enter().append('g')
-        .attr('class', className)
-        .merge(g)
-        .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-    const innerwidth = width - margin.left - margin.right;
-    const innerheight = height - margin.top - margin.bottom;
-
-    return { g, innerwidth, innerheight }
-}
-
-
-function piechart() {
 
 
 }
---
+++ Final Project/js/scripts.js
@@ -0,0 +1,76 @@
+// pie_chart()
+// // window.addEventListener('resize', pie_chart);
+
+// function pie_chart() {
+//     let div_id = "#world-asia-pie-chart"
+//     // Definition of the div target dimentions
+//     let ratio = 1.25; // 3 width = 1 height
+//     let win_width = d3.select(div_id).node().getBoundingClientRect().width;
+//     let win_height = win_width / ratio;
+
+//     // set the dimensions and margins of the graph
+//     let margin = 20,
+//         width = win_width - margin,
+//         height = win_height - margin;
+
+
+//     // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+//     const radius = Math.min(width, height) / 2 - margin
+
+//     // append the svg object to the div called 'my_dataviz'
+//     const svg = d3.select("#world-asia-pie-chart")
+//         .append("svg")
+//         .attr("width", width)
+//         .attr("height", height)
+//         .append("g")
+//         .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+//     // Create dummy data
+//     const data = { "a": 9, "b": 20, "c": 30, "d": 8, "e": 12, "f": 4, "h": 10 }
+
+//     // set the color scale
+
+//     var colorList = ["#ff595e", "#ffca3a", "#dd6e42", "#6a4c93",
+//         "#8ac926", "#1982c4", "#8f2d56"];
+//     // ["#FF1D33", "#26265C", "#ffb703", "#fb8500",
+//     //     "#084c61", "#8f2d56", "#1982c4", "#6a4c93", "#218380"];
+
+//     const color = d3.scaleOrdinal()
+//         .range(colorList);
+//     // .range(d3.schemeSet2);
+
+//     // Compute the position of each group on the pie:
+//     const pie = d3.pie()
+//         .value(function (d) {
+//             console.log(d[1])
+//             return d[1]
+//         })
+//     const data_ready = pie(Object.entries(data))
+//     // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+//     // shape helper to build arcs:
+//     const arcGenerator = d3.arc()
+//         .innerRadius(0)
+//         .outerRadius(radius)
+
+//     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+//     svg
+//         .selectAll('mySlices')
+//         .data(data_ready)
+//         .join('path')
+//         .attr('d', arcGenerator)
+//         .attr('fill', function (d) { return (color(d.data[0])) })
+//         .attr("stroke", "white")
+//         .style("stroke-width", "1px")
+//         .style("opacity", 0.9)
+
+//     // Now add the annotation. Use the centroid method to get the best coordinates
+//     svg
+//         .selectAll('mySlices')
+//         .data(data_ready)
+//         .join('text')
+//         .text(function (d) { return "grp " + d.data[0] })
+//         .attr("transform", function (d) { return `translate(${arcGenerator.centroid(d)})` })
+//         .style("text-anchor", "middle")
+//         .style("font-size", "1.25em")
+// }
